0bd2dbdcf952792aaa253ec2e5461d7fb88800fe
Hector Vargas <hvhdeveloper@gmail.com>
2020-08-31 20:43:08 -0600

    Aqui hay que revisar el addlistener del click porque reinicia contador

--- app.js
+++ app.js
@@ -7,66 +7,62 @@ function addElement(){
         "id": "id",
         "class": "this that and so on",
         "data-whatever": "whatever data",
-       });
+    });
 
+    divElem.setAttribute('class', 'divElem')
+    divElem.style.width = "100%"
+    document.body.appendChild(divElem);
 
-       porcentaje.setAttribute('class', 'aElem');
+    aElem.setAttribute('class', 'aElem');
+    aElem.setAttribute('href', 'javascript:window.open("http://hekvh.com")');
+    aElem.innerHTML = "DRINK ME";
+    aElem.style.marginLeft = "10px";
 
-       divElem.setAttribute('class','divElem')
-       divElem.style.width = "100%"
-       document.body.appendChild(divElem);
+    divElem.appendChild(aElem);
 
-       aElem.setAttribute('class', 'aElem');
-       aElem.setAttribute('href', 'javascript:window.open("http://hekvh.com")');
-       aElem.innerHTML = "DRINK ME";
-       aElem.style.marginLeft = "10px";
-       
-       divElem.appendChild(aElem);
+    porcentaje.setAttribute('class', 'aElem');
 
-       var eatMe = document.createElement("a");
-       eatMe.setAttribute('class','aElem');
-       eatMe.setAttribute('opacity','1');
-       eatMe.setAttribute('href', '');
-       eatMe.style.opacity = '1';
-       var opacidad = eatMe.style.opacity;
-       
-       var intervalo = setInterval(function(){ 
+    var eatMe = document.createElement("a");
+    eatMe.setAttribute('class', 'aElem');
+    eatMe.setAttribute('opacity', '1');
+    eatMe.setAttribute('href', '');
+    eatMe.style.opacity = '1';
+
+    eatMe.innerHTML = "EAT ME";
+    console.log("hola");
+    eatMe.style.marginLeft = "10px";
+    divElem.appendChild(eatMe);
+
+    porcentaje.setAttribute('href', 'javascript:window.open("http://hekvh.com")');
+    porcentaje.innerHTML = "100%";
+    porcentaje.style.marginLeft = "10px";
+    divElem.appendChild(porcentaje);
+
+    eatMe.addEventListener('click', () => {
+        alert(opacidad);
         
-            /*if(eatMe.style.visibility === 'hidden'){
-                eatMe.style.visibility = 'visible';
-            }  */
-                opacidad = eatMe.style.opacity;
-                opacidad = parseFloat(opacidad);
-                console.log(opacidad);
-                    opacidad = opacidad - 0.1
-                    console.log(opacidad);
+    })
+    
+    var opacidad = eatMe.style.opacity;
+    var intervalo = setInterval(function () {
 
-                    eatMe.style.opacity = opacidad.toString();
-                    console.log(eatMe.style.opacity);
-                    porcentaje.innerHTML = "  " + (eatMe.style.opacity * 100) + " %";
+        /*if(eatMe.style.visibility === 'hidden'){
+            eatMe.style.visibility = 'visible';
+        }  */
+        opacidad = eatMe.style.opacity;
+        opacidad = parseFloat(opacidad);
+        console.log(opacidad);
+        opacidad = opacidad - 0.1
+        console.log(opacidad);
 
-                    if (opacidad == 0){
-                        clearInterval(intervalo);
-                    }
-                    
-                //}
-                //eatMe.style.visibility = 'hidden';
-        }, 500);
+        eatMe.style.opacity = opacidad.toString();
+        console.log(eatMe.style.opacity);
+        porcentaje.innerHTML = "  " + (eatMe.style.opacity * 100) + " %";
 
-        eatMe.addEventListener('click', ()=>{
-            alert("hola");
-        })
-
-       eatMe.innerHTML = "EAT ME";
-       console.log("hola");
-       eatMe.style.marginLeft = "10px";
-       divElem.appendChild(eatMe);
-
-       porcentaje.setAttribute('href', 'javascript:window.open("http://hekvh.com")');
-       porcentaje.innerHTML = "100%";
-       porcentaje.style.marginLeft = "10px";
-       divElem.appendChild(porcentaje);
-
+        if (opacidad == 0) {
+            clearInterval(intervalo);
+        }
+    }, 500);
 
 }
 
